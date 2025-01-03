1870931cb76f317ea7a9a8d69691a4c00be6f0c6
martasavenok <magnolia.sav@gmail.com>
2025-01-03 22:05:45 +0300

    end

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,10 @@ const body = document.querySelector('body');
 const modeBtn = document.querySelector('.theme-mode');
 const lightModeIcon= document.querySelector('.light-mode-icon');
 const darkModeIcon= document.querySelector('.dark-mode-icon');
+const themeBtn = document.querySelector('.theme-button');
+const lightThemeIcon= document.querySelector('.light-theme-icon');
+const darkThemeIcon= document.querySelector('.dark-theme-icon');
+const themeBtnText = document.querySelector('.theme-button-text');
 
 modeBtn.addEventListener('click', (e)=> {
     if (body.classList.contains('light-theme')) {
@@ -51,3 +55,86 @@ modeBtn.addEventListener('click', (e)=> {
         darkModeIcon.classList.remove('active');
     }
 })
+themeBtn.addEventListener('click', (e)=> {
+    if (body.classList.contains('light-theme')) {
+        body.classList.add('dark-theme');
+        body.classList.remove('light-theme');
+        lightThemeIcon.classList.remove('active');
+        darkThemeIcon.classList.add('active');
+        themeBtnText.innerHTML = 'Светлая тема'
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        lightThemeIcon.classList.add('active');
+        darkThemeIcon.classList.remove('active');
+        themeBtnText.innerHTML = 'Темная тема'
+    }
+})
+
+// burger-menu
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuCloseBtn = document.querySelector('.mobile-menu .close-btn');
+const burgerBtn = document.querySelector('.burger-btn');
+const overlay = document.querySelector('.overlay');
+
+const showMobileMenu = () => {
+    mobileMenu.classList.add('active');
+    overlay.classList.add('open');
+}
+const hideMobileMenu = () => {
+    mobileMenu.classList.remove('active');
+    overlay.classList.remove('open');
+}
+mobileMenuCloseBtn.addEventListener('click', (e)=> {
+    hideMobileMenu()
+})
+overlay.addEventListener('click', (e)=> {
+    hideMobileMenu()
+})
+burgerBtn.addEventListener('click', (e)=> {
+    showMobileMenu()
+})
+
+const heroSwiper = document.querySelector('.hero-swiper');
+
+if (heroSwiper) {
+    const swiper = new Swiper(heroSwiper, {
+        loop: true,
+        pagination: {
+            el: '.custom-hero-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.custom-hero-navigation .arrow-next',
+            prevEl: '.custom-hero-navigation .arrow-prev',
+        },
+    });
+}
+
+const advantagesSwiper = document.querySelector('.advantages-swiper');
+if (advantagesSwiper) {
+    const swiper = new Swiper(advantagesSwiper, {
+        loop: true,
+        spaceBetween: 4,
+        slidesPerView: 1.3,
+        breakpoints: {
+            600: {
+                slidesPerView: 2.2,
+                spaceBetween: 20
+            },
+            830: {
+                slidesPerView: 2.5,
+                spaceBetween: 30
+            },
+            1150: {
+                slidesPerView: 3.4,
+                spaceBetween: 30
+            },
+            1350: {
+                slidesPerView: 4,
+                spaceBetween: 50
+            }
+        }
+    });
+}
